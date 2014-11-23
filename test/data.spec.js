07fb9c810ba168dc12d6c499983a134a6a34242d
Christopher Giroir <kelsin@valefor.com>
2014-11-22 20:25:53 -0800

    Initial tests

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,2 +1,28 @@
-var expect = require('chai').expect
-var sinon = require('sinon')
+'use strict';
+
+var expect = require('chai').expect;
+
+var data = require('../lib/data');
+
+describe('data.js', function() {
+
+  describe('getCurrentChain', function() {
+
+    it('should return null when there is no data', function() {
+      expect(data.getCurrentChain()).to.be.null;
+    });
+  });
+
+  describe('newChain', function() {
+
+    it('should return a new chain', function() {
+      var newChain = data.newChain();
+      expect(newChain.name).to.equal('New Chain');
+    });
+
+    it('should set the new chain as the current chain', function() {
+      var newChain = data.newChain();
+      expect(data.getCurrentChain()).to.equal(newChain);
+    });
+  });
+});
